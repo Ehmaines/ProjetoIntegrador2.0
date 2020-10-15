526f216dd23e9facbe75c124aa6d945367c2d1cb
Ehmaines <edumaines27@gmail.com>
2020-10-15 15:55:50 -0300

    Traducao da tela de registro

---
+++ resources/lang/en/register.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'registrarUsuario' => 'Register User',
+    'nome' => 'Name',
+    'email' => 'E-mail',
+    'senha' => 'Password',
+    'confirmarSenha' => 'Confirm Password',
+    'voltar' => 'Back',
+    'registrar' => 'Register',
+    'esqueceuSenha' => 'Forgot your Password?'
+    
+];
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,14 +5,14 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Registrar usuário</div>
+                    <div class="card-header">{{__('register.registrarUsuario')}}</div>
 
                     <div class="card-body">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
 
                             <div class="form-group row">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">Nome</label>
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{__('register.nome')}}</label>
 
                                 <div class="col-md-6">
                                     <input id="name" type="text"
@@ -29,7 +29,7 @@
 
                             <div class="form-group row">
                                 <label for="email"
-                                       class="col-md-4 col-form-label text-md-right">E-mail</label>
+                                       class="col-md-4 col-form-label text-md-right">{{__('register.email')}}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email"
@@ -46,7 +46,7 @@
 
                             <div class="form-group row">
                                 <label for="password"
-                                       class="col-md-4 col-form-label text-md-right">Senha</label>
+                                       class="col-md-4 col-form-label text-md-right">{{__('register.senha')}}</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password"
@@ -63,7 +63,7 @@
 
                             <div class="form-group row">
                                 <label for="password-confirm"
-                                       class="col-md-4 col-form-label text-md-right">Confirmar a senha</label>
+                                       class="col-md-4 col-form-label text-md-right">{{__('register.confirmarSenha')}}</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control"
@@ -73,9 +73,9 @@
 
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
-                                    <a href="{{route('welcome')}}" class="btn btn-primary">Voltar</a>  
+                                    <a href="{{route('welcome')}}" class="btn btn-primary">{{__('register.voltar')}}</a>  
                                     <button type="submit" class="btn btn-primary">
-                                        Registrar
+                                        {{__('register.registrar')}}
                                     </button>
                                 </div>
                             </div>
